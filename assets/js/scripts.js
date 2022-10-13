7d03c2dcf0c9313315c5e3c19840714658de2fa3
Jubstacheit <dessaintviviane@gmail.com>
2022-10-13 19:01:39 +0200

    edited burger color

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
 function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("burger-bar").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
+    document.getElementById("burger-bg").classList.toggle("change-bg");
 }
