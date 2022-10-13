343a8e5fe206445c44eb2e2752cfc1f62fefc5c3
Jubstacheit <dessaintviviane@gmail.com>
2022-10-13 19:51:25 +0200

    deleted burger

--- assets/js/scripts.js
+++
@@ -1,5 +0,0 @@
-function menuOnClick() {
-    document.getElementById("burger-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("burger-bg").classList.toggle("change-bg");
-}
