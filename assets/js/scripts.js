12b4d4bc90c2cae3015a5d03ac52ccb77cc09c6a
Jubstacheit <dessaintviviane@gmail.com>
2022-10-17 00:32:03 +0200

    added burger background and animation - need to change color

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,5 @@
 function burgerClick() {
     document.getElementById("burger").classList.toggle("burger-change");
     document.getElementById("burger-menu").classList.toggle("burger-menu-change");
+    document.getElementById("burger-menu-bg").classList.toggle("burger-menu-bg-change");
 }
